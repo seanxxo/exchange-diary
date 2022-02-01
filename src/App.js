2527150a18ec86ef51cbd786989b66d025da178f
Minyoung <sean_o@naver.com>
2022-02-01 17:47:36 +0900

    ✨ login view flow

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Routes, Route, Outlet } from "react-router-dom";
 import "./App.css";
 
@@ -6,18 +7,29 @@ function App() {
     <Routes>
       <Route element={<Layout />}>
         <Route path="*" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Route>
     </Routes>
   );
 }
 
-const Home = () => {
+const Login = ({ setIsLogin }) => {
+  setIsLogin();
   return <div></div>;
+};
+
+const MyDiary = () => {
+  return <div>마이 다이어리~</div>;
+};
+
+const Home = () => {
+  const [isLogin, setIsLogin] = useState(false);
+  return isLogin ? <MyDiary /> : <Login setIsLogin={setIsLogin} />;
 };
 
 const Layout = () => {
   return (
-    <div class="layout">
+    <div className="layout">
       <header></header>
       <main>
         <Outlet />
