46bb0fd24e0469646c44f3ce66c51adcde31a66a
Minyoung <sean_o@naver.com>
2022-02-06 15:22:55 +0900

    :recycle: CHANGE login status view flow

--- src/App.js
+++ src/App.js
@@ -1,49 +1,65 @@
 import { useEffect, useState } from "react";
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route, Outlet, useNavigate } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return (
+  const [isLogin, setIsLogin] = useState(false);
+  return isLogin ? (
     <Routes>
       <Route element={<Layout />}>
-        <Route path="*" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<MyDiary />} />
       </Route>
     </Routes>
+  ) : (
+    <Cover />
   );
 }
 
-const Login = ({ setIsLogin }) => {
-  useEffect(() => {
-    setIsLogin(true);
-  });
-  return <div>로그인하쉐이~</div>;
+const Cover = () => {
+  // 한번 해봤으니까 라우터로 바꾸기...
+  const [view, setView] = useState();
+  switch (view) {
+    case "join":
+      return <Join />;
+    case "login":
+      return <Login />;
+    default:
+      return (
+        <div>
+          <h3>비회원을 위한 홍보화면</h3>
+          <button onClick={() => setView("join")}>회원가입</button>
+          <button onClick={() => setView("login")}>로그인</button>
+        </div>
+      );
+  }
 };
 
-const Logout = ({ setIsLogin }) => {
-  useEffect(() => {
-    console.log(this);
-    setIsLogin(false);
-  });
-  return <div>로그아웃 되었음</div>;
+const Join = () => {
+  return <div>회원가입폼</div>;
 };
 
-const MyDiary = ({ setIsLogin }) => {
+const Login = () => {
   return (
     <div>
-      <h3>마이다이어리~</h3>
-      <button onClick={() => Logout(setIsLogin)}>로그아웃 하기~</button>
+      <h3>로그인 페이지</h3>
+      <button>로그인</button>
     </div>
   );
 };
 
-const Home = () => {
-  const [isLogin, setIsLogin] = useState(false);
-  return isLogin ? (
-    <MyDiary setIsLogin={setIsLogin} />
-  ) : (
-    <Login setIsLogin={setIsLogin} />
+const MyDiary = () => {
+  return (
+    <div>
+      <h3>마이다이어리~</h3>
+      <button onClick={() => Logout()}>로그아웃</button>
+    </div>
   );
+};
+
+const Logout = ({ setIsLogin }) => {
+  useEffect(() => {
+    setIsLogin(false);
+  });
 };
 
 const Layout = () => {
