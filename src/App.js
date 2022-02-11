2616118d1f7d841ea198572a94587fb80aec215d
Minyoung <sean_o@naver.com>
2022-02-11 16:03:52 +0900

    :recycle: fix handleclick

--- src/App.js
+++ src/App.js
@@ -1,28 +1,24 @@
-import React, { useContext, useEffect, useState } from "react";
-
-const LoginContext = React.createContext();
+import { useState } from "react";
 
 function App() {
   const [isLogin, setIsLogin] = useState(false);
 
-  return (
-    <LoginContext.Provider value={setIsLogin}>
-      {isLogin ? <MyDiary /> : <Cover />}
-    </LoginContext.Provider>
+  return isLogin ? (
+    <MyDiary setIsLogin={setIsLogin} />
+  ) : (
+    <Cover setIsLogin={setIsLogin} />
   );
 }
 
-// const useObserber = (hook, value) => {
-//   useEffect(hook(value));
-// };
-
-const Cover = () => {
+const Cover = ({ setIsLogin }) => {
   const [view, setView] = useState();
   const init = (
     <div>
       <h3>비회원을 위한 홍보화면</h3>
       <button onClick={() => setView(<Join />)}>회원가입</button>
-      <button onClick={() => setView(<Login />)}>로그인</button>
+      <button onClick={() => setView(<Login setIsLogin={setIsLogin} />)}>
+        로그인
+      </button>
     </div>
   );
   return view || init;
@@ -32,40 +28,32 @@ const Join = () => {
   return <div>회원가입폼</div>;
 };
 
-const Login = () => {
-  // const useClick = () => {
-  //   useContext(LoginContext)(true);
-  // };
+const Login = ({ setIsLogin }) => {
+  const login = () => {
+    setIsLogin(true);
+  };
   return (
     <div>
       <h3>로그인 페이지</h3>
-      {/* <button onClick={useClick}>로그인</button> */}
+      <button onClick={login}>로그인</button>
     </div>
   );
 };
 
-const MyDiary = () => {
+const MyDiary = ({ setIsLogin }) => {
   return (
     <div>
       <h3>마이다이어리~</h3>
-      <button onClick={() => Logout()}>로그아웃</button>
+      <Logout setIsLogin={setIsLogin} />
     </div>
   );
 };
 
 const Logout = ({ setIsLogin }) => {
-  useEffect(() => {
+  const logout = () => {
     setIsLogin(false);
-  });
-};
-
-const Layout = () => {
-  return (
-    <div className="layout">
-      <header></header>
-      <main>{/* <Outlet /> */}</main>
-    </div>
-  );
+  };
+  return <button onClick={logout}>로그아웃</button>;
 };
 
 export default App;
