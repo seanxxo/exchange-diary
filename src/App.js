58c24fdfef989667e7a9b57cae5cf65a651f0f40
Minyoung <sean_o@naver.com>
2022-02-13 22:29:56 +0900

    :sparkles: create dummy json

--- src/App.js
+++ src/App.js
@@ -10,12 +10,38 @@ function App() {
   );
 }
 
+const MyDiary = ({ setIsLogin }) => {
+  return (
+    <div>
+      <h3>마이다이어리~</h3>
+      <Logout setIsLogin={setIsLogin} />
+      <button>간단한 마이페이지</button>
+      <Content />
+    </div>
+  );
+};
+
+const Content = () => (
+  <div>
+    <h3>접속 첫 화면</h3>
+    <button>오늘 글 쓰기</button>
+    <div>n분 후 열람 가능...</div>
+    <ul>지난 글 목록</ul>
+  </div>
+);
+
 const Cover = ({ setIsLogin }) => {
   const [view, setView] = useState();
   const init = (
     <div>
       <h3>비회원을 위한 홍보화면</h3>
-      <button onClick={() => setView(<Join />)}>회원가입</button>
+      <button
+        onClick={() =>
+          setView(<Join setView={setView} setIsLogin={setIsLogin} />)
+        }
+      >
+        회원가입
+      </button>
       <button onClick={() => setView(<Login setIsLogin={setIsLogin} />)}>
         로그인
       </button>
@@ -24,36 +50,34 @@ const Cover = ({ setIsLogin }) => {
   return view || init;
 };
 
-const Join = () => {
-  return <div>회원가입폼</div>;
+const Join = ({ setView, setIsLogin }) => {
+  return (
+    <div>
+      <h3>회원가입폼</h3>
+      <button onClick={() => setView(<Login setIsLogin={setIsLogin} />)}>
+        가입완료
+      </button>
+    </div>
+  );
 };
 
 const Login = ({ setIsLogin }) => {
-  const login = () => {
+  const handleClick = () => {
     setIsLogin(true);
   };
   return (
     <div>
       <h3>로그인 페이지</h3>
-      <button onClick={login}>로그인</button>
-    </div>
-  );
-};
-
-const MyDiary = ({ setIsLogin }) => {
-  return (
-    <div>
-      <h3>마이다이어리~</h3>
-      <Logout setIsLogin={setIsLogin} />
+      <button onClick={handleClick}>로그인</button>
     </div>
   );
 };
 
 const Logout = ({ setIsLogin }) => {
-  const logout = () => {
+  const handleClick = () => {
     setIsLogin(false);
   };
-  return <button onClick={logout}>로그아웃</button>;
+  return <button onClick={handleClick}>로그아웃</button>;
 };
 
 export default App;
